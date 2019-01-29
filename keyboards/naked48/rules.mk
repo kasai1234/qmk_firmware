f18a325ccb2e4cc96652ae74b5ce5f58956c3a16
Salicylic-acid3 <46864619+Salicylic-acid3@users.noreply.github.com>
2019-01-29 23:12:25 +0900

    Update rules.mk

--- keyboards/naked48/rules.mk
+++ keyboards/naked48/rules.mk
@@ -51,7 +51,8 @@ BOOTLOADER = caterina
 OPT_DEFS += -DINTERRUPT_CONTROL_ENDPOINT
 
 # Build Options
-#   change yes to no to disable
+#   change to "no" to disable the options, or define them in the Makefile in
+#   the appropriate keymap folder that will get included automatically
 #
 BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration(+1000)
 MOUSEKEY_ENABLE = no       # Mouse keys(+4700)
