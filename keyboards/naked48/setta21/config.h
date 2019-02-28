6d8bdf1d62d27c24f6d83f0f7e9eda0be13cba3c
Salicylic-acid3 <46864619+Salicylic-acid3@users.noreply.github.com>
2019-03-01 07:00:25 +0900

    Update config.h

--- keyboards/naked48/setta21/config.h
+++ keyboards/naked48/setta21/config.h
@@ -1,6 +1,5 @@
 /*
-Copyright 2012 Jun Wako <wakojun@gmail.com>
-Copyright 2015 Jack Humbert
+Copyright 2018 Salicylic_Acid
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -20,71 +19,15 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "config_common.h"
 #include <serial_config.h>
-/* USB Device descriptor parameter */
-#define VENDOR_ID       0xFEED
-#define PRODUCT_ID      0x0000
-#define DEVICE_VER      0x0001
-#define MANUFACTURER    Salicylic_Acid
-#define PRODUCT         setta21
-#define DESCRIPTION     A custom tenkeypad
 
-/* key matrix size */
-#define MATRIX_ROWS 8
-#define MATRIX_COLS 12
+#define USE_I2C
+#define USE_SERIAL
 
-#define MATRIX_ROW_PINS { D4, C6, D7, E6 }
+#ifdef USE_Link_Time_Optimization
+  // LTO has issues with macros (action_get_macro) and "functions" (fn_actions),
+  //  so just disable them
+  #define NO_ACTION_MACRO
+  #define NO_ACTION_FUNCTION
 
-// wiring of each half
-#define MATRIX_COL_PINS { F4, F5, F6, F7, B1, B3, B2, B6, B4, B5, D1, D0 }
-// #define MATRIX_COL_PINS { D0, D1, B5, B4, D6, B2, B3, B1, F7, F6, F5, F4 } //uncomment this line and comment line above if you need to reverse left-to-right key order
-
-/*
-// Rows are doubled-up
-#define MATRIX_ROWS 8
-#define MATRIX_COLS 12
-
-#define MATRIX_ROW_PINS { D2, D3, D4, C6 }
-#define MATRIX_COL_PINS { F4, F5, F6, F7, B1, B3, B2, B6, D7, E6, B4, B5 }
-*/
-
-/* define if matrix has ghost */
-//#define MATRIX_HAS_GHOST
-
-/* number of backlight levels */
-// #define BACKLIGHT_LEVELS 3
-
-/* Set 0 if debouncing isn't needed */
-#define DEBOUNCING_DELAY 5
-
-/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
-//#define LOCKING_SUPPORT_ENABLE
-/* Locking resynchronize hack */
-//#define LOCKING_RESYNC_ENABLE
-
-/* key combination for command */
-#define IS_COMMAND() ( \
-    keyboard_report->mods == (MOD_BIT(KC_LSHIFT) | MOD_BIT(KC_RSHIFT)) \
-)
-
-/* ws2812 RGB LED */
-#define RGB_DI_PIN D3
-
-#define RGBLED_NUM 21    // Number of LEDs
-
-/*
- * Feature disable options
- *  These options are also useful to firmware size reduction.
- */
-
-/* disable debug print */
-// #define NO_DEBUG
-
-/* disable print */
-// #define NO_PRINT
-
-/* disable action features */
-//#define NO_ACTION_LAYER
-//#define NO_ACTION_TAPPING
-//#define NO_ACTION_ONESHOT
-//#define NO_ACTION_MACRO
-//#define NO_ACTION_FUNCTION
+  #define DISABLE_LEADER
+#endif // USE_Link_Time_Optimization
