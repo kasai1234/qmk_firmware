c687393d4bba53e2c6a517c948a6566af9d967da
Salicylic-acid3 <46864619+Salicylic-acid3@users.noreply.github.com>
2019-03-04 22:47:10 +0900

    Update rev1.c

--- keyboards/setta21/rev1/rev1.c
+++ keyboards/setta21/rev1/rev1.c
@@ -1,5 +1,9 @@
 #include "setta21.h"
 
+#ifdef AUDIO_ENABLE
+    float tone_startup[][2] = SONG(STARTUP_SOUND);
+    float tone_goodbye[][2] = SONG(GOODBYE_SOUND);
+#endif
 
 #ifdef SSD1306OLED
 
@@ -9,5 +13,18 @@ void led_set_kb(uint8_t usb_led) {
 
 void matrix_init_kb(void) {
 
+    #ifdef AUDIO_ENABLE
+        _delay_ms(20); // gets rid of tick
+        PLAY_SONG(tone_startup);
+    #endif
+
 	matrix_init_user();
 };
+
+void shutdown_kb(void) {
+    #ifdef AUDIO_ENABLE
+        PLAY_SONG(tone_goodbye);
+      	_delay_ms(150);
+      	stop_all_notes();
+    #endif
+}
