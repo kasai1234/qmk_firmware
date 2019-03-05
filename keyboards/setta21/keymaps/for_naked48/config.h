c8994e051701531d7773b868b9630b7d09f05896
Salicylic-acid3 <46864619+Salicylic-acid3@users.noreply.github.com>
2019-03-06 07:25:22 +0900

    Update config.h

--- keyboards/setta21/keymaps/for_naked48/config.h
+++ keyboards/setta21/keymaps/for_naked48/config.h
@@ -24,7 +24,7 @@
 // #define MASTER_RIGHT
 // #define EE_HANDS
 
-// #define SSD1306OLED
+#define SSD1306OLED
 
 #define USE_SERIAL_PD2
 
@@ -33,7 +33,7 @@
 
 #undef RGBLED_NUM
 #define RGBLIGHT_ANIMATIONS
-#define RGBLED_NUM 48
+#define RGBLED_NUM 21
 #define RGBLIGHT_LIMIT_VAL 120
 #define RGBLIGHT_HUE_STEP 10
 #define RGBLIGHT_SAT_STEP 17
