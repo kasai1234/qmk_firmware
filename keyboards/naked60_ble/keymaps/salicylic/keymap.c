c366c456be981fa38265399e1a71fb37285a83f0
Salicylic-acid3 <46864619+Salicylic-acid3@users.noreply.github.com>
2019-05-06 00:01:19 +0900

    Update keymap.c

--- keyboards/naked60_ble/keymaps/salicylic/keymap.c
+++ keyboards/naked60_ble/keymaps/salicylic/keymap.c
@@ -15,26 +15,6 @@
  */
 #include QMK_KEYBOARD_H
 #include "app_ble_func.h"
-/*#include "bootloader.h"
-#ifdef PROTOCOL_LUFA
-  #include "lufa.h"
-  #include "split_util.h"
-#endif
-
-extern keymap_config_t keymap_config;
-
-#ifdef RGBLIGHT_ENABLE
-//Following line allows macro to read current RGB settings
-extern rgblight_config_t rgblight_config;
-#endif
-
-extern uint8_t is_master;
-
-// Each layer gets a name for readability, which is then used in the keymap matrix below.
-// The underscores don't mean anything - you can have a layer called STUFF or any other name.
-// Layer names don't all need to be of the same length, obviously, and you can also skip them
-// entirely and just use numbers.
-*/
 
 enum layer_number {
   _QWERTY = 0,
@@ -44,8 +24,9 @@ enum layer_number {
 };
 
 enum custom_keycodes {
-  QWERTY = SAFE_RANGE,
-  LOWER,
+//  QWERTY = SAFE_RANGE,
+//  LOWER,
+  LOWER = SAFE_RANGE,
   RAISE,
   ADJUST,
   KANJI,
@@ -71,27 +52,26 @@ enum custom_keycodes {
   ENT_SLP,              /* Deep sleep mode                      */
 };
 
-//extern keymap_config_t keymap_config;
-
-// Fillers to make layering more clear
+// Layer Mode aliases
 #define KC_MLAD  MO(_ADJUST)
 
-#define KC_LOWER _LOWER
-#define KC_RAISE _RAISE
+#define KC_LOWER LOWER
+#define KC_RAISE RAISE
 
 #define KC______ KC_TRNS
 #define KC_XXXXX KC_NO
 #define KC_KANJI KANJI
 
 //#define KC_RST   RESET
-/*#define KC_LTOG  RGB_TOG
-#define KC_LHUI  RGB_HUI
-#define KC_LHUD  RGB_HUD
-#define KC_LSAI  RGB_SAI
-#define KC_LSAD  RGB_SAD
-#define KC_LVAI  RGB_VAI
-#define KC_LVAD  RGB_VAD
-#define KC_LMOD  RGB_MOD*/
+// #define KC_LRST  RGBRST
+// #define KC_LTOG  RGB_TOG
+// #define KC_LHUI  RGB_HUI
+// #define KC_LHUD  RGB_HUD
+// #define KC_LSAI  RGB_SAI
+// #define KC_LSAD  RGB_SAD
+// #define KC_LVAI  RGB_VAI
+// #define KC_LVAD  RGB_VAD
+// #define KC_LMOD  RGB_MOD
 #define KC_KNRM  AG_NORM
 #define KC_KSWP  AG_SWAP
 #define KC_DEBUG DEBUG
@@ -147,7 +127,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        LSFT,     A,     S,     D,     F,     G,                   H,     J,     K,     L,  MINS, JRBRC,\
   //|------+------+------+------+------+------|             |------+------+------+------+------+------|
       LCTRL,     Z,     X,     C,     V,     B,                   N,     M,  COMM,   DOT,  SLSH, JENUN,\
-  //|------+------+------+------+------+------|------+------+------+------+------+------+------|
+  //|------+------+------+------+------+------|------+------+------+------+------+------+------+------|
               LEFT, RIGHT,  LGUI,  MHEN,   LEN,  BSPC,   DEL,   RSP,  HENK,  LALT,    UP,  DOWN \
           //`----------------------------------------------------------------------------------'
   ),
@@ -162,7 +142,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       JQUOT,  HASH, JDQUO, JLPRN, JRPRN,   JAT,               XXXXX,    P4,    P5,    P6, _____,  JEQL,\
   //|------+------+------+------+------+------|             |------+------+------+------+------+------|
        JHAT,  PERC, JAMPR,  SCLN, JCLON, JPIPE,                  P0,    P1,    P2,    P3, JPLUS,   ENT,\
-  //|------+------+------+------+------+------|------+------+------+------+------+------+------|
+  //|------+------+------+------+------+------|------+------+------+------+------+------+------+------|
              _____, _____, _____,  ZKHK, LOWER, _____, _____,  MLAD,    P0,  PDOT, _____, _____ \
           //`----------------------------------------------------------------------------------'
   ),
@@ -177,22 +157,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        SF11,    F1,    F2,    F3,    F4,    F5,               XXXXX,  LEFT,  DOWN, RIGHT,  LSFT,   ENT,\
   //|------+------+------+------+------+------|             |------+------+------+------+------+------|
        SF12,    F6,    F7,    F8,    F9,   F10,               XXXXX, XXXXX, XXXXX, XXXXX,  PGDN, XXXXX,\
-  //|------+------+------+------+------+------|------+------+------+------+------+------+------|
+  //|------+------+------+------+------+------|------+------+------+------+------+------+------+------|
              _____, _____, _____, _____,  MLAD, _____, _____, RAISE, _____, _____, _____, _____ \
           //`----------------------------------------------------------------------------------'
   ),
 
 
-  [_ADJUST] = LAYOUT_kc( /* Base */
+  [_ADJUST] = LAYOUT_kc( 
   //,-----------------------------------------|             |-----------------------------------------.
         F11,    F1,    F2,    F3,    F4,    F5,                  F6,    F7,    F8,    F9,   F10,   F12,\
   //|------+------+------+------+------+------|             |------+------+------+------+------+------|
-        ESC,  BATT,  KNRM,  KSWP, XXXXX, XXXXX,               XXXXX, XXXXX, XXXXX, XXXXX, XXXXX,   CAD,\
+        ESC,  BATT,  KNRM,  KSWP, XXXXX, XXXXX,               XXXXX,  BTN1,  MS_U,  BTN2, XXXXX,   CAD,\
   //|------+------+------+------+------+------|             |------+------+------+------+------+------|
-       SF11,  BTON, USBON, BTID0, BTID2, BTID4,               XXXXX, XXXXX, XXXXX, XXXXX, XXXXX,  PSCR,\
+       SF11,  BTON, USBON, BTID0, BTID2, BTID4,               XXXXX,  MS_L,  MS_D,  MS_R, XXXXX,  PSCR,\
   //|------+------+------+------+------+------|             |------+------+------+------+------+------|
        SF12,  BTOF, USBOF, BTID1, BTID3, BTNEW,               XXXXX, XXXXX, XXXXX, XXXXX, XXXXX, APSCR,\
-  //|------+------+------+------+------+------|------+------+------+------+------+------+------|
+  //|------+------+------+------+------+------|------+------+------+------+------+------+------+------|
              _____, _____, _____, _____, _____,   RST,   RST, _____, _____, _____, _____, _____ \
           //`----------------------------------------------------------------------------------'
   )
@@ -212,14 +192,14 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   bool result = true;
 
   switch (keycode) {
-  // case LOWER:
-  //   update_change_layer(record->event.pressed, _LOWER, _RAISE, _ADJUST);
-  //   result = false;
-  //   break;
-  // case RAISE:
-  //   update_change_layer(record->event.pressed, _RAISE, _LOWER, _ADJUST);
-  //   result = false;
-  //   break;
+  case LOWER:
+    update_change_layer(record->event.pressed, _LOWER, _RAISE, _ADJUST);
+    result = false;
+    break;
+  case RAISE:
+    update_change_layer(record->event.pressed, _RAISE, _LOWER, _ADJUST);
+    result = false;
+    break;
   case KANJI:
     if (record->event.pressed) {
       if (keymap_config.swap_lalt_lgui == false) {
@@ -336,92 +316,3 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
   return result;
 }
-
-/*
-// define variables for reactive RGB
-bool TOG_STATUS = false;
-int RGB_current_mode;
-
-void persistent_default_layer_set(uint16_t default_layer) {
-  eeconfig_update_default_layer(default_layer);
-  default_layer_set(default_layer);
-}
-
-// Setting ADJUST layer RGB back to default
-void update_tri_layer_RGB(uint8_t layer1, uint8_t layer2, uint8_t layer3) {
-  if (IS_LAYER_ON(layer1) && IS_LAYER_ON(layer2)) {
-    layer_on(layer3);
-  } else {
-    layer_off(layer3);
-  }
-}
-
-void matrix_init_user(void) {
-    #ifdef RGBLIGHT_ENABLE
-      RGB_current_mode = rgblight_config.mode;
-    #endif
-}
-
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  switch (keycode) {
-    case QWERTY:
-      if (record->event.pressed) {
-        // when keycode QMKBEST is pressed
-        persistent_default_layer_set(1UL<<_QWERTY);
-      }
-      return false;
-      break;
-    case LOWER:
-      if (record->event.pressed) {
-        layer_on(_LOWER);
-        //update_tri_layer_RGB(_LOWER, _RAISE, _ADJUST);
-      } else {
-        layer_off(_LOWER);
-        //update_tri_layer_RGB(_LOWER, _RAISE, _ADJUST);
-      }
-      return false;
-      break;
-    case RAISE:
-      if (record->event.pressed) {
-        layer_on(_RAISE);
-        //update_tri_layer_RGB(_LOWER, _RAISE, _ADJUST);
-      } else {
-        layer_off(_RAISE);
-        //update_tri_layer_RGB(_LOWER, _RAISE, _ADJUST);
-      }
-      return false;
-      break;
-    case ADJUST:
-        if (record->event.pressed) {
-          layer_on(_ADJUST);
-        } else {
-          layer_off(_ADJUST);
-        }
-        return false;
-        break;
-      //led operations - RGB mode change now updates the RGB_current_mode to allow the right RGB mode to be set after reactive keys are released
-    case RGB_MOD:
-      #ifdef RGBLIGHT_ENABLE
-        if (record->event.pressed) {
-          rgblight_mode(RGB_current_mode);
-          rgblight_step();
-          RGB_current_mode = rgblight_config.mode;
-        }
-      #endif
-      return false;
-      break;
-  }
-  return true;
-}
-*/
-
-/*
- *void matrix_scan_user(void) {
- *
- *}
- *
- *void led_set_user(uint8_t usb_led) {
- *
- *}
-*/
